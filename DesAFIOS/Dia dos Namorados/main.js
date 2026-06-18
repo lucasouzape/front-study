7d1dbcecbf50b936dd1c7eb5965bc8d8eda9bbf3
Lucas de Souza Pegoretti <lucasouzape@gmail.com>
2026-06-18 14:57:58 -0300

    feat: adiciona formulario de namorados com pegadinha js

---
+++ DesAFIOS/Dia dos Namorados/main.js
@@ -0,0 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const opcaoComCerteza = document.getElementById('icomctz');
+    
+    
+    const opcoesNegativas = document.querySelectorAll('.armadilha input');
+
+    opcoesNegativas.forEach(function(checkboxNegativo) {
+        checkboxNegativo.addEventListener('click', function(event) {
+            
+          
+            event.preventDefault(); 
+            
+    
+            opcaoComCerteza.checked = true; 
+            
+        });
+    });
+});
